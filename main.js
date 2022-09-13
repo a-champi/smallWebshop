8a478610f98ed496d316238048d710611aea6510
a-champi <a.champi.h@gmail.com>
2022-09-13 14:43:12 +0200

    finish

--- main.js
+++ main.js
@@ -12,80 +12,79 @@ document.addEventListener("click", (e) => {
     if(e.target.matches(".card .btn-primary")){
         addShoppingCard(e);    
     }
-    if(e.target.matches(".list-group-item btn-danger")){
+    if(e.target.matches("#shoppingCard .list-group-item .btn-success")){
         btnIncr(e);
     }
-    if(e.target.matches(".list-group-item btn-success")){
+    if(e.target.matches("#shoppingCard .list-group-item .btn-danger")){
         btnDecr(e);
     }
+    
 });
 
 let shoppingCardObject = [];
 
 const addShoppingCard = (e) => {
     //console.log(e.target.dataset.vegetable);
-    
     const product = {
-        id: e.target.dataset.vegetable, 
-        item: e.target.dataset.vegetable,
+        
+        id: e.target.dataset.vegetable,
+        title: e.target.dataset.vegetable,
         qty: 1,
         price: parseInt(e.target.dataset.price) 
     }
     
     const index = shoppingCardObject.findIndex(item => item.id === product.id)
-    
-    //checks if the product(object) is already in the container, 
-    //otherwise it will be put into the shopping cart
+    //checks if the product(object) is already in the container, otherwise it will be put into the shopping cart
     if(index === -1){
         shoppingCardObject.push(product);
     }else{
         shoppingCardObject[index].qty++;        
     }
-    
-    console.log(shoppingCardObject);
+        
     showShoppingCard();
     
 };
 
 const showShoppingCard = () => {
+    
     shoppingCard.textContent = "";
     
     //Object.values(shoppingCardObject).forEach((item) => {
         shoppingCardObject.forEach((item) => {    
         const clone = templateList.content.cloneNode(true);//first..., aufruf untergeordnete Knoten
-
-        clone.querySelector("text-white, .lead").textContent = item.title;// ruft script und css
-        clone.querySelector(".badge").textContent = item.qty;
+         
+        clone.querySelector("#position .lead").textContent = item.title;
+        clone.querySelector("#position .badge").textContent = item.qty;
         clone.querySelector("div .lead span").textContent = item.price * item.qty;
 
         clone.querySelector(".btn-success").dataset.id = item.id;
         clone.querySelector(".btn-danger").dataset.id = item.id;
-
+          
         fragment.appendChild(clone);//append..., fügt eien Konoten am Ende der Liste der Kinder..
+            
     });
-
+    
     shoppingCard.appendChild(fragment);// push to shopingCard
     showFooter() //push only one mal
-
 };
 
 
-const showFooter = () =>{
-    
+const showFooter = () =>{   
     footer.textContent = "";
-
     const total = shoppingCardObject.reduce((acc,current) =>
         acc + current.qty * current.price, 0 );
     
     const clone = templateFooter.content.cloneNode(true);
+    if(total !==0){
     clone.querySelector("span").textContent = total;
-
+    }else{
+       return footer.textContent ="";
+    }
     footer.appendChild(clone);
-
-}
+};
 
 const btnIncr = (e) => {
- 
+    console.log(" increement", shoppingCardObject) 
  shoppingCardObject = shoppingCardObject.map(item => {
     if(item.id === e.target.dataset.id ){
         item.qty++;
@@ -95,8 +94,9 @@ const btnIncr = (e) => {
  showShoppingCard();
 };
 
-const btnDecr = (e) => {
-    console.log("first")
+
+const btnDecr = (e) => {   
+    console.log(" Decreement", shoppingCardObject)
     shoppingCardObject = shoppingCardObject.filter(item  =>  {
         if(item.id === e.target.dataset.id){
             if(item.qty > 0){
